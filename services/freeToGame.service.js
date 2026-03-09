a16d5a61abcb6b9f13e0c5b6e5b7e89cfc37c41b
Kim Dudenhöfer <kidu2400@student.miun.se>
2026-03-09 12:25:36 +0100

    Lagt till service med anrop till externt api.

--- services/freeToGame.service.js
+++ services/freeToGame.service.js
@@ -1,2 +1,27 @@
 "use strict"
 
+exports.freeToGame = async(endpoint) => {
+
+    //Setting url
+    let url;
+
+    if(endpoint !== null) {
+        url = "https://www.freetogame.com/api/" + endpoint;
+    } else {
+        url = "https://www.freetogame.com/api/games";
+    }
+
+    try {
+        const response = await fetch(url);
+
+        if(!response.ok) {
+            throw new Error("An error occurred while fetchinge external data.");
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch(error) {
+        throw error;
+    }
+}
